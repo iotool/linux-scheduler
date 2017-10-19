60b679a03e0d7becf876f477ca5958951de6038e
iotool <roha.mbox@gmail.com>
2017-10-19 16:16:38 +0200

    Add files via upload

---
+++ step/step1.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# 
+# Copyright 2017 by IOTool under GNU Lesser General Public License v3.0
+# https://github.com/iotool/linux-scheduler/blob/master/LICENSE
+# 
+# Scheduler - Step mit Programmaufrufen
+# schedulerdir : Hauptverzeichnis vom Scheduler
+
+schedulerdir=/home/myuser/scheduler
+
+echo "step1";
+
